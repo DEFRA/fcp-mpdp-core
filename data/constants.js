dd2e746965595e78b9afabbab427c6f1540d73d9
Rana Salem <ranatasalem@gmail.com>
2025-09-12 09:46:54 +0100

    remove new entrants pilot from seed script

--- data/constants.js
+++ data/constants.js
@@ -1,7 +1,6 @@
 export const schemes = [
   'Farming Resilience Fund',
   'Landscape Recovery',
-  'New Entrants Pilot',
   'Sustainable Farming Incentive',
   'Countryside Stewardship',
   'Farming Investment Fund'
